bc00dd40498f01002cec80954cdfd3fd865201c8
Ashish <vagh.ashish@gmail.com>
2023-12-31 21:55:04 -0500

    - created MVC project of online Pie Shop - added layout, view imports and did styling - added EF Core to the application, created the context - creating and initializing the database - created necessary models and repositories related to Pie, Category and Order - added navigation to the site - created the Home page - created the shopping cart - added order form and validation for the form

---
+++ Models/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AshishPieShop.Models
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        public readonly AppDbContext _appDbContext;
+        public CategoryRepository(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IEnumerable<Category> AllCategories => _appDbContext.Categories;
+    }
+}
---
+++ Models/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AshishPieShop.Models
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
