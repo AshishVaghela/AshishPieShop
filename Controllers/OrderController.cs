42e296794cc3c4859dcd0ecb2baacaab124141cb
Ashish Vaghela <vagh.ashish@gmail.com>
2024-02-01 17:05:21 -0500

    - added scaffolding of identity in the project - overridden Login, Register and Logout screen - enabled Authorization

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -1,4 +1,5 @@
 ﻿using AshishPieShop.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace AshishPieShop.Controllers
 {
+    [Authorize]
     public class OrderController : Controller
     {
         private readonly IOrderRepository _orderRepository;
